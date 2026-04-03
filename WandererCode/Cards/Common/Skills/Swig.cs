582225fac77787a8a10417a303bd7da22aad157b
zeqzero <willis.thomas.j@gmail.com>
2026-04-02 20:10:57 -0700

    swig

---
+++ WandererCode/Cards/Common/Skills/Swig.cs
@@ -0,0 +1,39 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using Wanderer.WandererCode.Character;
+using Wanderer.WandererCode.Commands;
+
+namespace Wanderer.WandererCode.Cards;
+
+[Pool(typeof(WandererCardPool))]
+public class Swig : WandererCard
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [ new CardsVar(1), new DynamicVar("Transform", 1) ];
+
+    public Swig() : base(1, CardType.Skill, CardRarity.Common, TargetType.Self)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Cast", Owner.Character.CastAnimDelay);
+        await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.BaseValue, Owner);
+
+        var prefs = new CardSelectorPrefs(CardSelectorPrefs.TransformSelectionPrompt, 1);
+        var card = (await CardSelectCmd.FromHand(choiceContext, Owner, prefs, null, this)).FirstOrDefault();
+
+        if (card != null)
+        {
+            await WandererCmd.TransformToRandomFromPool(card, Owner);
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Cards.UpgradeValueBy(1m);
+    }
+}
--- WandererCode/Cards/Common/Skills/Weave.cs
+++ WandererCode/Cards/Common/Skills/Weave.cs
@@ -32,6 +32,6 @@ public class Weave : WandererCard
 
     protected override void OnUpgrade()
     {
-        base.DynamicVars.Cards.UpgradeValueBy(1m);
+        DynamicVars.Cards.UpgradeValueBy(1m);
     }
 }
