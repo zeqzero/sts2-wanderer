47d1052b1926fb2dc10aa0b53bb28dd59c24a5c5
zeqzero <willis.thomas.j@gmail.com>
2026-04-04 17:14:42 -0700

    flaunt shame

--- WandererCode/Cards/Token/Ofuda.cs
+++ WandererCode/Cards/Token/Ofuda.cs
@@ -1,21 +1,19 @@
 using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Entities.Cards;
-using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.CardPools;
-using Wanderer.WandererCode.Commands;
 
 namespace Wanderer.WandererCode.Cards;
 
 /// <tags>shift, exhaust</tags>
-[Pool(typeof(TokenCardPool))]
+[Pool(typeof(StatusCardPool))]
 public class Ofuda : WandererCard
 {
-    public override CardPoolModel Pool => ModelDb.CardPool<TokenCardPool>();
+    public override CardPoolModel Pool => ModelDb.CardPool<StatusCardPool>();
 
     public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
 
-    public Ofuda() : base(0, CardType.Skill, CardRarity.Token, TargetType.Self)
+    public Ofuda() : base(0, CardType.Status, CardRarity.Token, TargetType.Self)
     {
     }
 }
---
+++ WandererCode/Cards/Uncommon/Attacks/FlauntShame.cs
@@ -0,0 +1,49 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using Wanderer.WandererCode.Character;
+using Wanderer.WandererCode.Keywords;
+
+namespace Wanderer.WandererCode.Cards;
+
+/// <tags>death</tags>
+[Pool(typeof(WandererCardPool))]
+public class FlauntShame : WandererCard
+{
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [ WandererKeywords.Enshrined ];
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(5m, ValueProp.Move)];
+
+    public FlauntShame() : base(3, CardType.Attack, CardRarity.Uncommon, TargetType.AllEnemies)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(CombatState, "CombatState");
+
+        var statusCards = PileType.Hand.GetPile(Owner).Cards.Where(c => c.Type == CardType.Status).ToList();
+
+        foreach (var card in statusCards)
+        {
+            await CardCmd.Discard(choiceContext, card);
+        }
+
+        if (statusCards.Count > 0)
+        {
+            await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+                .WithHitCount(statusCards.Count)
+                .FromCard(this)
+                .TargetingAllOpponents(CombatState)
+                .Execute(choiceContext);
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(3m);
+    }
+}
