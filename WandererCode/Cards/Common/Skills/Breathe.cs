9f1ca48516ad8f1af8b914d927615cd94d0d7fc0
zeqzero <willis.thomas.j@gmail.com>
2026-04-07 19:32:53 -0700

    breathe

---
+++ WandererCode/Cards/Common/Skills/Breathe.cs
@@ -0,0 +1,46 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Hooks;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using Wanderer.WandererCode.Character;
+using Wanderer.WandererCode.Powers;
+
+namespace Wanderer.WandererCode.Cards;
+
+/// <tags>counter</tags>
+[Pool(typeof(WandererCardPool))]
+public class Breathe : WandererCard
+{
+    public override bool GainsBlock => true;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new BlockVar(6m, ValueProp.Move),
+        new BlockVar("BlockNextTurn", 9m, ValueProp.Move)
+    ];
+    
+    public Breathe() : base(2, CardType.Skill, CardRarity.Common, TargetType.Self)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(CombatState);
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Cast", Owner.Character.CastAnimDelay);
+        BlockVar blockVar = (BlockVar)DynamicVars["BlockNextTurn"];
+        IEnumerable<AbstractModel> modifiers;
+        decimal blockNextTurnAmount = Hook.ModifyBlock(CombatState, Owner.Creature, blockVar.BaseValue, blockVar.Props, this, cardPlay, out modifiers);
+        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+        await PowerCmd.Apply<WandererNextTurnBlockPower>(Owner.Creature, blockNextTurnAmount, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Block.UpgradeValueBy(2);
+        DynamicVars["BlockNextTurn"].UpgradeValueBy(2);
+    }
+}
---
+++ WandererCode/Powers/WandererNextTurnBlockPower.cs
@@ -0,0 +1,55 @@
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Wanderer.WandererCode.Powers;
+
+public sealed class WandererNextTurnBlockPower : WandererPower, IWandererNextTurnPower
+{
+    public override PowerType Type => PowerType.Buff;
+
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override bool AllowNegative => true;
+
+    public override async Task AfterSideTurnStart(CombatSide side, CombatState combatState)
+    {
+        if (side == Owner.Side && AmountOnTurnStart != 0)
+        {
+            if (Amount >= 0)
+            {
+                await CreatureCmd.GainBlock(Owner, Amount, ValueProp.Unpowered, null);
+            }
+            else
+            {
+                await CreatureCmd.LoseBlock(Owner, Amount);
+            }
+
+            if (Amount == AmountOnTurnStart)
+            {
+                await PowerCmd.Remove(this);
+            }
+            else
+            {
+                await PowerCmd.Apply(this, Owner, -AmountOnTurnStart, Owner, null);
+            }
+        }
+    }
+
+    public async Task ApplyNow(PlayerChoiceContext choiceContext, Player player)
+    {
+        if (Amount >= 0)
+        {
+            await CreatureCmd.GainBlock(Owner, Amount, ValueProp.Unpowered, null);
+        }
+        else
+        {
+            await CreatureCmd.LoseBlock(Owner, Amount);
+        }
+
+        await PowerCmd.Remove(this);
+    }
+}
