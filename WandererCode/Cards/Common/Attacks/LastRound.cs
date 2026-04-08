01a9f2e01901449ec58efdd767b436923d09e04f
zeqzero <willis.thomas.j@gmail.com>
2026-04-07 20:50:08 -0700

    last round

---
+++ WandererCode/Cards/Common/Attacks/LastRound.cs
@@ -0,0 +1,39 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using Wanderer.WandererCode.Character;
+using Wanderer.WandererCode.Commands;
+
+namespace Wanderer.WandererCode.Cards;
+
+/// <tags>shift, exhaust</tags>
+[Pool(typeof(WandererCardPool))]
+public class LastRound : WandererCard
+{
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [ new DamageVar(10, ValueProp.Move) ];
+
+    public LastRound() : base(1, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target, "cardPlay.Target");
+
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this).Targeting(cardPlay.Target)
+            .WithHitFx("vfx/vfx_attack_slash", null, "slash_attack.mp3")
+            .Execute(choiceContext);
+
+        await WandererCmd.PickAndShiftCardsFromHand(choiceContext, 1, Owner, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(3);
+    }
+}
