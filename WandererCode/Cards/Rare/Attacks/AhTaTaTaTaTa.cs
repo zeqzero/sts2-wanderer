162cebe708eff9d3553cf5769135622d0025a07c
zeqzero <willis.thomas.j@gmail.com>
2026-04-07 18:27:54 -0700

    ahtatatatata

---
+++ WandererCode/Cards/Rare/Attacks/AhTaTaTaTaTa.cs
@@ -0,0 +1,44 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using Wanderer.WandererCode.Character;
+
+namespace Wanderer.WandererCode.Cards;
+
+/// <tags>flurry</tags>
+[Pool(typeof(WandererCardPool))]
+public class AhTaTaTaTaTa : WandererCard
+{
+    protected override bool HasEnergyCostX => true;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(1m, ValueProp.Move)];
+
+    public AhTaTaTaTaTa() : base(0, CardType.Attack, CardRarity.Rare, TargetType.RandomEnemy)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(CombatState, "CombatState");
+
+        int hitCount = ResolveEnergyXValue();
+
+        await DamageCmd
+            .Attack(DynamicVars.Damage.BaseValue)
+            .WithHitCount(hitCount)
+            .FromCard(this)
+            .TargetingRandomOpponents(CombatState)
+            .WithHitFx("vfx/vfx_attack_slash")
+            .Execute(choiceContext);
+
+        BaseReplayCount += hitCount;
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(1);
+    }
+}
