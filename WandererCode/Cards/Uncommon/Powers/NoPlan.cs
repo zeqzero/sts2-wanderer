cea93a15690a424cede80525c5da07ad76b062a8
zeqzero <willis.thomas.j@gmail.com>
2026-04-09 14:57:58 -0700

    no plan

---
+++ WandererCode/Cards/Uncommon/Powers/NoPlan.cs
@@ -0,0 +1,33 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using Wanderer.WandererCode.Character;
+using Wanderer.WandererCode.Powers;
+
+namespace Wanderer.WandererCode.Cards;
+
+/// <tags>shift</tags>
+[Pool(typeof(WandererCardPool))]
+public class NoPlan : WandererCard
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<NoPlanPower>(3)];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<NoPlanPower>()];
+
+    public NoPlan() : base(1, CardType.Power, CardRarity.Uncommon, TargetType.Self)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await PowerCmd.Apply<NoPlanPower>(Owner.Creature, DynamicVars["NoPlanPower"].BaseValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars["NoPlanPower"].UpgradeValueBy(2m);
+    }
+}
---
+++ WandererCode/Powers/NoPlanPower.cs
@@ -0,0 +1,18 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Wanderer.WandererCode.Powers;
+
+public class NoPlanPower : WandererPower
+{
+    public override PowerType Type => PowerType.Buff;
+
+    public override PowerStackType StackType => PowerStackType.None;
+
+    public override async Task AfterShifted(CardModel card)
+    {
+        await CreatureCmd.GainBlock(Owner, Amount, ValueProp.Unpowered, null);
+    }
+}
