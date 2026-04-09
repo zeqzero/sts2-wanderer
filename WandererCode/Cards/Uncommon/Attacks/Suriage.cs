55fbefbbd09cb573a44ebd5d2522941986e3e129
zeqzero <willis.thomas.j@gmail.com>
2026-04-09 15:09:08 -0700

    suriage

---
+++ WandererCode/Cards/Uncommon/Attacks/Suriage.cs
@@ -0,0 +1,46 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using Wanderer.WandererCode.Character;
+
+namespace Wanderer.WandererCode.Cards;
+
+[Pool(typeof(WandererCardPool))]
+public class Suriage : WandererCard
+{
+    public override bool GainsBlock => true;
+
+    protected override bool ShouldGlowGoldInternal =>
+        CombatState?.HittableEnemies.Any(e => e.Monster?.IntendsToAttack ?? false) ?? false;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new DamageVar(5m, ValueProp.Move),
+        new BlockVar(8m, ValueProp.Move)
+    ];
+
+    public Suriage() : base(1, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target, "cardPlay.Target");
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this).Targeting(cardPlay.Target)
+            .WithHitFx("vfx/vfx_attack_slash", null, "slash_attack.mp3")
+            .Execute(choiceContext);
+
+        if (cardPlay.Target.Monster.IntendsToAttack)
+        {
+            await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Block.UpgradeValueBy(4m);
+    }
+}
