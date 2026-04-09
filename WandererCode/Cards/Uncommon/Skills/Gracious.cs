c4652ddeff842dd86f6e24164530a2a7a3f797b4
zeqzero <willis.thomas.j@gmail.com>
2026-04-09 11:47:45 -0700

    gracious

---
+++ WandererCode/Cards/Uncommon/Skills/Gracious.cs
@@ -0,0 +1,43 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using Wanderer.WandererCode.Character;
+using Wanderer.WandererCode.Powers;
+
+namespace Wanderer.WandererCode.Cards;
+
+/// <tags>nextturn, draw</tags>
+[Pool(typeof(WandererCardPool))]
+public class Gracious : WandererCard
+{
+    public override bool GainsBlock => true;
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Innate, CardKeyword.Exhaust];
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(10m, ValueProp.Move), new CardsVar(2)];
+
+    public Gracious() : base(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
+    {
+    }
+
+    private async Task Talk()
+    {
+        TalkCmd.Play(LocString.GetRandomWithPrefix("characters", "WANDERER-WANDERER-GRACIOUS"), Owner.Creature);
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await Talk();
+        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+        await PowerCmd.Apply<WandererNextTurnDrawPower>(Owner.Creature, DynamicVars.Cards.BaseValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Block.UpgradeValueBy(3m);
+    }
+}
