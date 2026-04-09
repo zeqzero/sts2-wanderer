2aa4f276e53c319fb94bd273d5d7f91c2a04f4e0
zeqzero <willis.thomas.j@gmail.com>
2026-04-09 11:59:07 -0700

    prudence

---
+++ WandererCode/Cards/Uncommon/Skills/Prudence.cs
@@ -0,0 +1,35 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using Wanderer.WandererCode.Character;
+
+namespace Wanderer.WandererCode.Cards;
+
+/// <tags>gold</tags>
+[Pool(typeof(WandererCardPool))]
+public class Prudence : WandererCard
+{
+    public override bool GainsBlock => true;
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(5m, ValueProp.Move), new DynamicVar("Gold", 5m)];
+
+    public Prudence() : base(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+        await PlayerCmd.GainGold(DynamicVars["Gold"].IntValue, Owner);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Block.UpgradeValueBy(3m);
+    }
+}
