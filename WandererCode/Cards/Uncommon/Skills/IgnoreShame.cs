774fb61e7aa42ba5e5876b58295ceb791f7bfa0c
zeqzero <willis.thomas.j@gmail.com>
2026-04-09 11:09:25 -0700

    ignore shame

---
+++ WandererCode/Cards/Uncommon/Skills/IgnoreShame.cs
@@ -0,0 +1,43 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Combat.History.Entries;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using Wanderer.WandererCode.Character;
+
+namespace Wanderer.WandererCode.Cards;
+
+/// <tags>exhaust, counter</tags>
+[Pool(typeof(WandererCardPool))]
+public class IgnoreShame : WandererCard
+{
+    public override bool GainsBlock => true;
+
+    protected override bool ShouldGlowGoldInternal => WasCardExhaustedThisTurn;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(10m, ValueProp.Move)];
+
+    private bool WasCardExhaustedThisTurn => CombatManager.Instance.History.Entries
+        .OfType<CardExhaustedEntry>()
+        .Any(e => e.HappenedThisTurn(CombatState) && e.Card.Owner == Owner);
+
+    public IgnoreShame() : base(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (WasCardExhaustedThisTurn)
+        {
+            await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Block.UpgradeValueBy(3m);
+    }
+}
