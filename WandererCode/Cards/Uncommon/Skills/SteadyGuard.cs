f2b7993dfadbbf3fb864dc977062617ec021208b
zeqzero <willis.thomas.j@gmail.com>
2026-04-08 18:40:44 -0700

    steady guard

---
+++ WandererCode/Cards/Uncommon/Skills/SteadyGuard.cs
@@ -0,0 +1,39 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using Wanderer.WandererCode.Character;
+using Wanderer.WandererCode.Commands;
+
+namespace Wanderer.WandererCode.Cards;
+
+/// <tags>stance</tags>
+[Pool(typeof(WandererCardPool))]
+public class SteadyGuard : WandererCard
+{
+    public override bool GainsBlock => true;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(13m, ValueProp.Move)];
+
+    public SteadyGuard() : base(2, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+    }
+
+    public override async Task AfterStanceLeft(Creature creature, Stance oldStance)
+    {
+        DynamicVars.Block.BaseValue -= 2m;
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Block.UpgradeValueBy(4m);
+    }
+}
