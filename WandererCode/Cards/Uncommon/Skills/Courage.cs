8113dc188cbd4deaf8587873ceb3804a84a9261c
zeqzero <willis.thomas.j@gmail.com>
2026-04-09 11:55:39 -0700

    courage

---
+++ WandererCode/Cards/Uncommon/Skills/Courage.cs
@@ -0,0 +1,35 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+using Wanderer.WandererCode.Character;
+
+namespace Wanderer.WandererCode.Cards;
+
+/// <tags>vuln</tags>
+[Pool(typeof(WandererCardPool))]
+public class Courage : WandererCard
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<VulnerablePower>(2)];
+
+    public Courage() : base(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await PowerCmd.Apply<VulnerablePower>(Owner.Creature, DynamicVars["VulnerablePower"].BaseValue, Owner.Creature, this);
+        foreach (var enemy in CombatState.HittableEnemies)
+        {
+            await PowerCmd.Apply<VulnerablePower>(enemy, DynamicVars["VulnerablePower"].BaseValue, Owner.Creature, this);
+        }
+        await PowerCmd.Remove<WeakPower>(Owner.Creature);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars["VulnerablePower"].UpgradeValueBy(1m);
+    }
+}
