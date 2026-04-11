2111b1bc3f03215b3b5e4c3583b91fc9e511fadd
zeqzero <willis.thomas.j@gmail.com>
2026-04-11 15:18:06 -0700

    rooted strike

---
+++ WandererCode/Cards/Uncommon/Attacks/RootedStrike.cs
@@ -0,0 +1,43 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using Wanderer.WandererCode.Character;
+using Wanderer.WandererCode.Commands;
+
+namespace Wanderer.WandererCode.Cards;
+
+/// <tags>commit</tags>
+[Pool(typeof(WandererCardPool))]
+public class RootedStrike : WandererCard
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [ new DamageVar(7m, ValueProp.Move), new DynamicVar("ReapplyAmount", 1) ];
+
+    protected override HashSet<CardTag> CanonicalTags => new HashSet<CardTag> { CardTag.Strike };
+
+    public RootedStrike() : base(1, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target, "cardPlay.Target");
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this).Targeting(cardPlay.Target)
+            .WithHitFx("vfx/vfx_attack_slash")
+            .Execute(choiceContext);
+
+        var stancePower = WandererCmd.GetCurrentStancePower(Owner.Creature);
+        if (stancePower is PowerModel power)
+        {
+            await PowerCmd.Apply(power, Owner.Creature, DynamicVars["ReapplyAmount"].BaseValue, Owner.Creature, this);
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars["ReapplyAmount"].UpgradeValueBy(1);
+    }
+}
