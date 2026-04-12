31bd8563a0e207638d81f08b65c49ba33247dc8a
zeqzero <willis.thomas.j@gmail.com>
2026-04-12 15:21:48 -0700

    kekkai

---
+++ WandererCode/Cards/Rare/Skills/Kekkai.cs
@@ -0,0 +1,42 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using Wanderer.WandererCode.Character;
+using Wanderer.WandererCode.Keywords;
+
+namespace Wanderer.WandererCode.Cards;
+
+/// <tags>shift</tags>
+[Pool(typeof(WandererCardPool))]
+public class Kekkai : WandererCard
+{
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.FromKeyword(CardKeyword.Exhaust)
+    ];
+
+    public Kekkai() : base(0, CardType.Skill, CardRarity.Rare, TargetType.Self)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        var selected = await CardSelectCmd.FromHand(choiceContext, Owner, new CardSelectorPrefs(SelectionScreenPrompt, 1),
+            c => !c.Keywords.Contains(WandererKeywords.Enshrined), this);
+
+        foreach (var card in selected)
+        {
+            card.AddKeyword(WandererKeywords.Enshrined);
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        RemoveKeyword(CardKeyword.Exhaust);
+    }
+}
