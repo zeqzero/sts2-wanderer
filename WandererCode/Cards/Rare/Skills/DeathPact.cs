bdd21b07a2b8e312b81f128e6e14032aca504fce
zeqzero <willis.thomas.j@gmail.com>
2026-04-12 12:49:45 -0700

    death pact (untested 😎)

---
+++ WandererCode/Cards/Rare/Skills/DeathPact.cs
@@ -0,0 +1,54 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models.Powers;
+using Wanderer.WandererCode.Character;
+using Wanderer.WandererCode.Powers;
+
+namespace Wanderer.WandererCode.Cards;
+
+/// <tags>death, multiplayer</tags>
+[Pool(typeof(WandererCardPool))]
+public class DeathPact : WandererCard
+{
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.FromKeyword(CardKeyword.Exhaust),
+        HoverTipFactory.FromPower<IntangiblePower>()
+    ];
+
+    public override CardMultiplayerConstraint MultiplayerConstraint => CardMultiplayerConstraint.MultiplayerOnly;
+
+    public DeathPact() : base(1, CardType.Skill, CardRarity.Rare, TargetType.Self)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Cast", Owner.Character.CastAnimDelay);
+
+        if (CombatState == null) return;
+
+        foreach (var player in CombatState.Players)
+        {
+            if (player != Owner && player.Creature.IsAlive)
+            {
+                await PowerCmd.Apply<DeathPactPower>(player.Creature, 1, Owner.Creature, this);
+                var power = player.Creature.GetPower<DeathPactPower>();
+                if (power != null)
+                {
+                    power.Source = Owner.Creature;
+                }
+            }
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        EnergyCost.UpgradeBy(-1);
+    }
+}
---
+++ WandererCode/Powers/DeathPactPower.cs
@@ -0,0 +1,35 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models.Powers;
+
+namespace Wanderer.WandererCode.Powers;
+
+public class DeathPactPower : WandererPower
+{
+    public override PowerType Type => PowerType.Buff;
+
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public Creature? Source { get; set; }
+
+    public override async Task AfterDeath(PlayerChoiceContext choiceContext, Creature creature, bool wasRemovalPrevented, float deathAnimLength)
+    {
+        if (creature != Source) return;
+
+        Flash();
+        await PowerCmd.Apply<IntangiblePower>(Owner, Amount, Source, null);
+        await PowerCmd.Remove(this);
+    }
+
+    public override async Task BeforeRitualDeath(Creature creature)
+    {
+        if (creature != Source) return;
+
+        Flash();
+        await PowerCmd.Apply<IntangiblePower>(Owner, Amount, Source, null);
+        await PowerCmd.Remove(this);
+    }
+
+}
