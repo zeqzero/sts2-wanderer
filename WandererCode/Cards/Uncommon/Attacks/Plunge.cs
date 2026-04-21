1667f9b2431c2b31b7c353610659d554ceae4034
zeqzero <willis.thomas.j@gmail.com>
2026-04-21 13:30:37 -0700

    plunge rework as counter/draw

--- WandererCode/Cards/Uncommon/Attacks/Plunge.cs
+++ WandererCode/Cards/Uncommon/Attacks/Plunge.cs
@@ -3,21 +3,21 @@ using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
 using Wanderer.WandererCode.Character;
-using Wanderer.WandererCode.Powers;
 
 namespace Wanderer.WandererCode.Cards;
 
-/// <tags>nextturn, draw</tags>
+/// <tags>draw, counter</tags>
 /// <art>wanderer after sticking their katana all the way through an enemy, only the hilt is seen, the end of the katana visible on the other side of the foe</art>
 /// <kanji>貫</kanji>
 [Pool(typeof(WandererCardPool))]
 public class Plunge : WandererCard
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(15m, ValueProp.Move), new CardsVar(2)];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [ new DamageVar(6m, ValueProp.Move) ];
 
-    public Plunge() : base(2, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy)
+    public Plunge() : base(1, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy)
     {
     }
 
@@ -28,11 +28,16 @@ public class Plunge : WandererCard
             .WithHitFx("vfx/vfx_attack_slash")
             .Execute(choiceContext);
 
-        await PowerCmd.Apply<WandererNextTurnDrawPower>(Owner.Creature, DynamicVars.Cards.BaseValue, Owner.Creature, this);
+        CardModel? drawn;
+        do
+        {
+            drawn = await CardPileCmd.Draw(choiceContext, Owner);
+        }
+        while (drawn != null && drawn.Type == CardType.Skill && CardPile.GetCards(Owner, PileType.Hand).Count() < 10);
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVars.Damage.UpgradeValueBy(5m);
+        DynamicVars.Damage.UpgradeValueBy(3m);
     }
 }
