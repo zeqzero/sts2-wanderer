2a5419eaf05f6ef63e23544480f07244ce536579
zeqzero <willis.thomas.j@gmail.com>
2026-04-23 11:30:33 -0700

    zangeki rework

--- WandererCode/Cards/Rare/Attacks/Zangeki.cs
+++ WandererCode/Cards/Rare/Attacks/Zangeki.cs
@@ -1,5 +1,6 @@
 using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Commands.Builders;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
@@ -8,18 +9,22 @@ using Wanderer.WandererCode.Character;
 
 namespace Wanderer.WandererCode.Cards;
 
-/// <tags>aoe</tags>
+/// <tags>aoe, counter, flurry</tags>
 /// <art>wanderer sheathing sword, several action lines through slain enemies indicating many instantaneous high power attacks</art>
 /// <kanji>斬撃</kanji>
 [Pool(typeof(WandererCardPool))]
 public class Zangeki : WandererCard
 {
+    public override bool GainsBlock => true;
+
+    protected override bool HasEnergyCostX => true;
+
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
-        new DamageVar(24m, ValueProp.Move)
+        new DamageVar(5m, ValueProp.Move)
     ];
 
-    public Zangeki() : base(3, CardType.Attack, CardRarity.Rare, TargetType.AllEnemies)
+    public Zangeki() : base(0, CardType.Attack, CardRarity.Rare, TargetType.AllEnemies)
     {
     }
 
@@ -27,13 +32,21 @@ public class Zangeki : WandererCard
     {
         ArgumentNullException.ThrowIfNull(CombatState, "CombatState");
 
-        await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this).TargetingAllOpponents(CombatState)
+        int hitCount = ResolveEnergyXValue();
+
+        AttackCommand attackCommand = await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .WithHitCount(hitCount)
+            .FromCard(this)
+            .TargetingAllOpponents(CombatState)
             .WithHitFx("vfx/vfx_attack_blunt", null, "heavy_attack.mp3")
             .Execute(choiceContext);
+
+        int totalDamage = attackCommand.Results.Sum(r => r.TotalDamage + r.OverkillDamage);
+        await CreatureCmd.GainBlock(Owner.Creature, totalDamage, ValueProp.Move, cardPlay);
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVars.Damage.UpgradeValueBy(7m);
+        DynamicVars.Damage.UpgradeValueBy(3m);
     }
 }
