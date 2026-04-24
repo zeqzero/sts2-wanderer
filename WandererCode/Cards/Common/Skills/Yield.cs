12fbb3775ef735de78d24134ff39fa0a08b53389
zeqzero <willis.thomas.j@gmail.com>
2026-04-23 20:02:18 -0700

    kamae hovertip

--- WandererCode/Cards/Common/Skills/Yield.cs
+++ WandererCode/Cards/Common/Skills/Yield.cs
@@ -2,6 +2,7 @@ using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.ValueProps;
 using Wanderer.WandererCode.Character;
@@ -18,6 +19,8 @@ public class Yield : WandererCard
     public override bool GainsBlock => true;
 
     protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(5m, ValueProp.Move), new CardsVar(1)];
+
+    protected override IEnumerable<IHoverTip> WandererExtraHoverTips => [HoverTipFactory.FromCard<Kamae>()];
 
     public Yield() : base(1, CardType.Skill, CardRarity.Common, TargetType.Self)
     {
