5e871531496511374092cdbb9d181d1e29741d19
zeqzero <willis.thomas.j@gmail.com>
2026-04-24 08:37:56 -0700

    count regrets rework

--- WandererCode/Cards/Uncommon/Skills/CountRegrets.cs
+++ WandererCode/Cards/Uncommon/Skills/CountRegrets.cs
@@ -2,13 +2,13 @@ using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
-using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
 using Wanderer.WandererCode.Character;
 using Wanderer.WandererCode.Keywords;
 
 namespace Wanderer.WandererCode.Cards;
 
-/// <tags>death</tags>
+/// <tags>ofuda, enshrined</tags>
 /// <art>wanderer face palming, tight zoom on head</art>
 /// <kanji>悔</kanji>
 [Pool(typeof(WandererCardPool))]
@@ -16,19 +16,23 @@ public class CountRegrets : WandererCard
 {
     public override IEnumerable<CardKeyword> CanonicalKeywords => [ WandererKeywords.Enshrined ];
 
-    protected override IEnumerable<DynamicVar> CanonicalVars => [ new CardsVar(2) ];
-
-    public CountRegrets() : base(3, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
+    public CountRegrets() : base(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
     {
     }
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.BaseValue, Owner);
+        int openSlots = 10 - PileType.Hand.GetPile(Owner).Cards.Count();
+        var ofudas = new List<CardModel>();
+        for (int i = 0; i < openSlots; i++)
+        {
+            ofudas.Add(CombatState.CreateCard<Ofuda>(Owner));
+        }
+        await CardPileCmd.AddGeneratedCardsToCombat(ofudas, PileType.Hand, addedByPlayer: true);
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVars.Cards.UpgradeValueBy(2m);
+        EnergyCost.UpgradeBy(-1);
     }
 }
