40c9cb7d1037566b983f144db5b4984e711feec6
zeqzero <willis.thomas.j@gmail.com>
2026-04-26 08:59:45 -0700

    buff retaliate

--- WandererCode/Cards/Common/Skills/Retaliate.cs
+++ WandererCode/Cards/Common/Skills/Retaliate.cs
@@ -4,6 +4,7 @@ using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
 using Wanderer.WandererCode.Character;
 using Wanderer.WandererCode.Powers;
 
@@ -15,21 +16,22 @@ namespace Wanderer.WandererCode.Cards;
 [Pool(typeof(WandererCardPool))]
 public class Retaliate : WandererCard
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<RetaliatePower>(1)];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [ new BlockVar(3, ValueProp.Move), new PowerVar<RetaliatePower>(1) ];
 
     protected override IEnumerable<IHoverTip> WandererExtraHoverTips => [HoverTipFactory.FromPower<CounterPower>()];
 
-    public Retaliate() : base(1, CardType.Skill, CardRarity.Common, TargetType.Self)
+    public Retaliate() : base(0, CardType.Skill, CardRarity.Common, TargetType.Self)
     {
     }
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
+        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block.BaseValue, ValueProp.Move, cardPlay);
         await PowerCmd.Apply<RetaliatePower>(Owner.Creature, DynamicVars["RetaliatePower"].BaseValue, Owner.Creature, this);
     }
 
     protected override void OnUpgrade()
     {
-        EnergyCost.UpgradeBy(-1);
+        DynamicVars.Block.UpgradeValueBy(3);
     }
 }
